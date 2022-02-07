ca16477f661cf53c4f63da5ada71b08ff7eb7639
Nolan Beihl <nolan.beihl@gmail.com>
2022-02-07 15:39:36 -0700

    Fixed views on explorer to reflect corret syntax for a post request

--- drf_jwt_capstone_backend/explorers/views.py
+++ drf_jwt_capstone_backend/explorers/views.py
@@ -1,3 +1,4 @@
+from ast import Return
 from django.contrib.auth.models import User
 from rest_framework import serializers, status
 from rest_framework.views import APIView
@@ -14,9 +15,21 @@ class ExplorerList(APIView):
     permission_classes = [AllowAny]
 
     def post(self,request):
+        serializer = ExplorerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # explorers = Explorer.objects.all()
+        # serializer = ExplorerSerializer(explorers, many=True)
+        # return Response(serializer.data)
+
+    def get(self,request):
         explorers = Explorer.objects.all()
         serializer = ExplorerSerializer(explorers, many=True)
         return Response(serializer.data)
+
 
 
 # Create your views here.
